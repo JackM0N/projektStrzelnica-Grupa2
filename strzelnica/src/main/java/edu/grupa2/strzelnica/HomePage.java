8455dafa563e1932f7b076fe555946eccbb4be5c
JackM0N <przemyslawgotowala@gmail.com>
2024-04-01 15:40:10 +0200

    first commit

---
+++ strzelnica/src/main/java/edu/grupa2/strzelnica/HomePage.java
@@ -0,0 +1,14 @@
+package edu.grupa2.strzelnica;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomePage {
+
+    @GetMapping("/")
+    public String index() {
+        return "Aplikacja springboot działa poprawnie!";
+    }
+
+}
---
+++ strzelnica/src/main/java/edu/grupa2/strzelnica/StrzelnicaApplication.java
@@ -0,0 +1,13 @@
+package edu.grupa2.strzelnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrzelnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrzelnicaApplication.class, args);
+	}
+
+}
