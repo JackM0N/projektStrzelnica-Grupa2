216551a47bea05a5821ecb534c3ba89c29bf18e6
User <zaba495@gmail.com>
2024-04-06 15:28:11 +0200

    fixed init Angular

--- strzelnicaAngular/src/app/app.module.ts
+++ strzelnicaAngular/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NewsComponent } from './components/news.component';
 import { CommonModule } from '@angular/common';
-import { NewsFormComponent } from './components/newsFormComponent';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
-    NewsFormComponent
   ],
   imports: [
     CommonModule,
