fe757f317eddec7cf3fa32c53dcc6de54fbbd4e6
Stanislaw <31638@uniwersytetkaliski.edu.pl>
2024-04-13 20:12:32 +0200

    Added About page

--- strzelnicaAngular/src/app/app-routing.module.ts
+++ strzelnicaAngular/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewsComponent } from './components/news.component';
 import { NewsFormComponent } from './components/newsform.component';
+import { AboutComponent } from './components/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'news', pathMatch: 'full' },
   { path: 'news', component: NewsComponent },
   { path: 'news/add', component: NewsFormComponent },
   { path: 'news/edit/:id', component: NewsFormComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
