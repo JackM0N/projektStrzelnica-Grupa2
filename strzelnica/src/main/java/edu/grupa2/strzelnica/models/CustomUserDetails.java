c415128217442a15281708f2ce8ddea56da806ba
JackM0N <przemyslawgotowala@gmail.com>
2024-05-30 19:13:45 +0200

    Modified JWT token to extend its lifespan and add users id to the token

---
+++ strzelnica/src/main/java/edu/grupa2/strzelnica/models/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package edu.grupa2.strzelnica.models;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    @Getter
+    private Long id;
+    private String email;
+    private String password;
+    private List<Role> roles;
+
+    public CustomUserDetails(Users user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.roles = user.getRoles();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- strzelnica/src/main/java/edu/grupa2/strzelnica/security/JWTGenerator.java
+++ strzelnica/src/main/java/edu/grupa2/strzelnica/security/JWTGenerator.java
@@ -1,11 +1,14 @@
 package edu.grupa2.strzelnica.security;
 
+import edu.grupa2.strzelnica.models.CustomUserDetails;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 
@@ -16,16 +19,28 @@ import java.util.Date;
 public class JWTGenerator {
 
     public String generateToken(Authentication auth) {
-        String email = auth.getName();
+        CustomUserDetails userDetails = (CustomUserDetails)auth.getPrincipal();
+        String email = userDetails.getUsername();
+        Long id = userDetails.getId();
         Date now = new Date();
         Date expiration = new Date(now.getTime() + SecurityConstants.JWT_TOKEN_EXPIRATION_TIME);
 
         return Jwts.builder()
                 .subject(email)
+                .claim("id", id)
                 .issuedAt(new Date())
                 .expiration(expiration)
                 .signWith(getKeyFromToken())
                 .compact();
+    }
+
+    public Long getIdFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getKeyFromToken())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.get("id", Long.class);
     }
 
     public String getUsernameFromToken(String token) {
